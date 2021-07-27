f26bc7a290068faec70789bb10fa2a6f6d7d351d
Schulzkafer <gatsby886@gmail.com>
2021-07-27 18:48:47 -0300

    Updates

--- app/Container/painelControle/betCalc.js
+++
@@ -1,7 +0,0 @@
-export default function (line, credit, betCoef) {
-   let b;
-   if (betCoef < 11) b = betCoef * line;
-   else if (betCoef < 19) b = betCoef * 5 * line - 40 * line;
-   else if (betCoef < 23) b = betCoef * 10 * line - 130 * line;
-   return b > credit ? { b: line, bC: 1 } : { b: b, bC: betCoef };
-}
